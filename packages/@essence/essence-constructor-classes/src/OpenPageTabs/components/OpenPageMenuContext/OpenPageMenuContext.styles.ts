433ed338476fdd5e454eaabce816793e6e544547
Ivan Artemov <honyrik@gmail.com>
2019-10-04 22:16:28 +0800

    fix

--- packages/@essence/essence-constructor-classes/src/OpenPageTabs/components/OpenPageMenuContext/OpenPageMenuContext.styles.ts
+++ packages/@essence/essence-constructor-classes/src/OpenPageTabs/components/OpenPageMenuContext/OpenPageMenuContext.styles.ts
@@ -1,9 +1,9 @@
 import {IEssenceTheme} from "@essence/essence-constructor-share";
 import {makeStyles} from "@material-ui/core/styles";
-import tabMenuContextStyleDark from "./OpenPageMenuContextDark.styles";
-import tabMenuContextStyleLight from "./OpenPageMenuContextLight.styles";
+import openPageMenuContextDark from "./OpenPageMenuContextDark.styles";
+import openPageMenuContextLight from "./OpenPageMenuContextLight.styles";
 
 export const useStyles = makeStyles(
     (theme: IEssenceTheme) =>
-        theme.palette.type === "light" ? tabMenuContextStyleLight(theme) : tabMenuContextStyleDark(theme),
+        theme.palette.type === "light" ? openPageMenuContextDark(theme) : openPageMenuContextLight(theme),
 );

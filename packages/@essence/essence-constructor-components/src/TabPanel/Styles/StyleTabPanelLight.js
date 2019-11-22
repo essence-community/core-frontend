2f9b6f42b39d07732733f5f585404581b7e1369e
Oleh Kutsenko <inthedark1225@gmail.com>
2019-11-22 12:35:17 +0200

    CORE-1389 improved styles for light tab

--- packages/@essence/essence-constructor-components/src/TabPanel/Styles/StyleTabPanelLight.js
+++ packages/@essence/essence-constructor-components/src/TabPanel/Styles/StyleTabPanelLight.js
@@ -1,125 +1,5 @@
-export const StyleTabPanelLight = (theme) => ({
-    activeTabRoot: {
-        backgroundColor: "#fff0e1",
-        borderRight: `2px solid ${theme.palette.secondary.main}`,
-        borderTop: `2px solid ${theme.palette.secondary.main}`,
-    },
-    "content-left-hbox": {
-        borderTop: `2px solid ${theme.palette.secondary.main}`,
-    },
-    "content-left-vbox": {
-        borderTop: `2px solid ${theme.palette.secondary.main}`,
-    },
-    "content-right-hbox": {
-        borderTop: `2px solid ${theme.palette.secondary.main}`,
-    },
-    "content-right-vbox": {
-        borderTop: `2px solid ${theme.palette.secondary.main}`,
-    },
-    popoverButton: {
-        "&$popoverButtonActive": {
-            "&:before": {
-                backgroundColor: "#fff0e1",
-            },
-        },
-        "&$popoverButtonOpen": {
-            "&:hover": {
-                borderBottomColor: "transparent",
-                borderRadius: "3px 3px 0 0",
-            },
-        },
-        "&:before": {
-            borderLeft: `1px solid ${theme.palette.secondary.main}`,
-            borderTop: `1px solid ${theme.palette.secondary.main}`,
-            borderTopLeftRadius: 6,
-            bottom: 0,
-            // eslint-disable-next-line quotes
-            content: '""',
-            left: -5,
-            position: "absolute",
-            right: -10,
-            top: 0,
-            transform: "skewX(-30deg)",
-            zIndex: -1,
-        },
-        "&:hover": {
-            "&:before": {
-                // Reset on touch devices, it doesn't add specificity
-                "@media (hover: none)": {
-                    backgroundColor: "transparent",
-                },
-                backgroundColor: "#e9ecf4",
-            },
-            backgroundColor: "transparent",
-        },
-        borderRight: `1px solid ${theme.palette.secondary.main}`,
-        color: theme.palette.primary.main,
-        height: 36,
-        position: "relative",
-        width: 36,
-    },
-    popoverButtonActive: {},
-    popoverButtonOpen: {},
-    popoverButtonPaper: {
-        "&:before": {
-            backgroundColor: theme.palette.secondary.main,
-            // eslint-disable-next-line quotes
-            content: '""',
-            height: 1,
-            left: 2,
-            position: "absolute",
-            right: 50,
-            top: 0,
-        },
-        border: `1px solid ${theme.palette.secondary.main}`,
-        borderRadius: "3px 0 3px 3px",
-        borderTop: "none",
-    },
-    selectedTabRoot: {
-        "&$tabRoot": {
-            background: "#e9ecf4",
-        },
-    },
-    "tabItem-center-hbox": {
-        width: "100%",
-    },
-    "tabItem-left-hbox": {
-        borderRight: `2px solid ${theme.palette.secondary.main}`,
-    },
-    "tabItem-left-vbox": {
-        borderRight: `2px solid ${theme.palette.secondary.main}`,
-    },
-    "tabItem-right-hbox": {
-        borderLeft: `1px solid ${theme.palette.secondary.main}`,
-    },
-    "tabItem-right-vbox": {
-        borderLeft: `1px solid ${theme.palette.secondary.main}`,
-    },
-    tabRoot: {
-        "&:hover": {
-            background: "#e9ecf4",
-        },
-        borderRight: `1px solid ${theme.palette.secondary.main}`,
-        borderTop: `1px solid ${theme.palette.secondary.main}`,
-        borderTopRightRadius: 6,
-        flexShrink: 1,
-        height: 36,
-        minHeight: 36,
-        minWidth: 70,
-        transformOrigin: "top",
-    },
-    tabWrapper: {
-        display: "block",
-        fontSize: 13,
-        fontWeight: "bold",
-        overflow: "hidden",
-        padding: "0 4px",
-        textOverflow: "ellipsis",
-        textTransform: "none",
-        whiteSpace: "nowrap",
-        width: "100%",
-    },
-    "tabsFlexContainer-center-hbox": {
+export const StyleTabPanelLight = (theme) => {
+    const tabsFlexContainerCenter = {
         "& $tabRoot": {
             marginRight: 15,
             transform: "skewX(30deg)",
@@ -127,90 +7,246 @@ export const StyleTabPanelLight = (theme) => ({
         "& $tabWrapper": {
             transform: "skewX(-30deg)",
         },
-        borderBottom: `2px solid ${theme.palette.secondary.main}`,
+        borderBottom: `1px solid ${theme.palette.secondary.main}`,
         flexDirection: "row-reverse",
         justifyContent: "flex-end",
         marginTop: 4,
-    },
-    "tabsFlexContainer-left-hbox": {
-        "& $tabRoot": {
-            borderBottom: `1px solid ${theme.palette.secondary.main}`,
-            borderLeft: `1px solid ${theme.palette.secondary.main}`,
-            borderRight: "none",
-            borderTopLeftRadius: 6,
-            borderTopRightRadius: 0,
-            marginLeft: 20,
-            marginRight: -20,
-            transform: "skewX(-30deg)",
+    };
+
+    return {
+        activeTabRoot: {},
+        "content-left-hbox": {
+            borderTop: `1px solid ${theme.palette.secondary.main}`,
         },
-        "& $tabWrapper": {
-            textAlign: "end",
-            transform: "skewX(30deg)",
+        "content-left-vbox": {
+            borderTop: `1px solid ${theme.palette.secondary.main}`,
         },
-        flexDirection: "column-reverse",
-        width: 264,
-    },
-    "tabsFlexContainer-left-vbox": {
-        "& $tabRoot": {
-            borderLeft: `1px solid ${theme.palette.secondary.main}`,
-            borderRight: 0,
-            borderTopLeftRadius: 6,
-            borderTopRightRadius: 0,
-            height: "auto",
-            marginBottom: 15,
-            minWidth: 36,
-            padding: "10px 0",
-            transform: "skewY(-30deg)",
+        "content-right-hbox": {
+            borderTop: `1px solid ${theme.palette.secondary.main}`,
+        },
+        "content-right-vbox": {
+            borderTop: `1px solid ${theme.palette.secondary.main}`,
+        },
+        popoverButton: {
+            "&$popoverButtonActive": {
+                "&:before": {
+                    backgroundColor: "#fff0e1",
+                },
+            },
+            "&$popoverButtonOpen": {
+                "&:hover": {
+                    borderBottomColor: "transparent",
+                    borderRadius: "3px 3px 0 0",
+                },
+            },
+            "&:before": {
+                borderLeft: `1px solid ${theme.palette.secondary.main}`,
+                borderTop: `1px solid ${theme.palette.secondary.main}`,
+                borderTopLeftRadius: 6,
+                bottom: 0,
+                // eslint-disable-next-line quotes
+                content: '""',
+                left: -5,
+                position: "absolute",
+                right: -10,
+                top: 0,
+                transform: "skewX(-30deg)",
+                zIndex: -1,
+            },
+            "&:hover": {
+                "&:before": {
+                    // Reset on touch devices, it doesn't add specificity
+                    "@media (hover: none)": {
+                        backgroundColor: "transparent",
+                    },
+                    backgroundColor: "#e9ecf4",
+                },
+                backgroundColor: "transparent",
+            },
+            borderRight: `1px solid ${theme.palette.secondary.main}`,
+            color: theme.palette.primary.main,
+            height: 36,
+            position: "relative",
             width: 36,
         },
-        "& $tabWrapper": {
-            maxHeight: 234,
-            textAlign: "start",
-            transform: "skewY(30deg) rotate(180deg)",
-            writingMode: "vertical-rl",
+        popoverButtonActive: {},
+        popoverButtonOpen: {},
+        popoverButtonPaper: {
+            "&:before": {
+                backgroundColor: theme.palette.secondary.main,
+                // eslint-disable-next-line quotes
+                content: '""',
+                height: 1,
+                left: 2,
+                position: "absolute",
+                right: 50,
+                top: 0,
+            },
+            border: `1px solid ${theme.palette.secondary.main}`,
+            borderRadius: "3px 0 3px 3px",
+            borderTop: "none",
         },
-        flexDirection: "column-reverse",
-        paddingTop: 10,
-        width: 36,
-    },
-    "tabsFlexContainer-right-hbox": {
-        "& $tabRoot": {
-            borderBottom: `1px solid ${theme.palette.secondary.main}`,
-            marginLeft: -20,
-            transform: "skewX(30deg)",
+        selectedTabRoot: {
+            "&$tabRoot": {
+                background: "#e9ecf4",
+            },
         },
-        "& $tabWrapper": {
-            textAlign: "start",
-            transform: "skewX(-30deg)",
+        "tabItem-center-hbox": {
+            width: "100%",
         },
-        flexDirection: "column-reverse",
-        width: 264,
-    },
-    "tabsFlexContainer-right-vbox": {
-        "& $tabRoot": {
-            height: "auto",
-            marginBottom: 15,
-            minWidth: 36,
-            padding: "10px 0",
-            transform: "skewY(30deg)",
+        "tabItem-left-hbox": {
+            borderRight: `1px solid ${theme.palette.secondary.main}`,
+        },
+        "tabItem-left-vbox": {
+            borderRight: `1px solid ${theme.palette.secondary.main}`,
+        },
+        "tabItem-right-hbox": {
+            borderLeft: `1px solid ${theme.palette.secondary.main}`,
+        },
+        "tabItem-right-vbox": {
+            borderLeft: `1px solid ${theme.palette.secondary.main}`,
+        },
+        tabRoot: {
+            "&$activeTabRoot": {
+                backgroundColor: "#fff0e1",
+                borderRight: `2px solid ${theme.palette.secondary.main}`,
+                borderTop: `2px solid ${theme.palette.secondary.main}`,
+            },
+            "&:hover": {
+                background: "#e9ecf4",
+            },
+            borderRight: `1px solid ${theme.palette.secondary.main}`,
+            borderTop: `1px solid ${theme.palette.secondary.main}`,
+            borderTopRightRadius: 6,
+            flexShrink: 1,
+            height: 36,
+            minHeight: 36,
+            minWidth: 70,
+            transformOrigin: "top",
+        },
+        tabWrapper: {
+            display: "block",
+            fontSize: 13,
+            fontWeight: "bold",
+            overflow: "hidden",
+            padding: "0 4px",
+            textOverflow: "ellipsis",
+            textTransform: "none",
+            whiteSpace: "nowrap",
+            width: "100%",
+        },
+        "tabsFlexContainer-center-hbox": tabsFlexContainerCenter,
+        "tabsFlexContainer-center-vbox": tabsFlexContainerCenter,
+        "tabsFlexContainer-left-hbox": {
+            "& $tabRoot": {
+                "&$activeTabRoot": {
+                    borderLeft: `2px solid ${theme.palette.secondary.main}`,
+                    borderTop: `2px solid ${theme.palette.secondary.main}`,
+                },
+                "&:last-child": {
+                    "&$activeTabRoot": {
+                        borderTop: `2px solid ${theme.palette.secondary.main}`,
+                    },
+                    borderTop: `1px solid ${theme.palette.secondary.main}`,
+                },
+                borderBottom: `1px solid ${theme.palette.secondary.main}`,
+                borderLeft: `1px solid ${theme.palette.secondary.main}`,
+                borderRight: "none",
+                borderTop: "none",
+                borderTopLeftRadius: 6,
+                borderTopRightRadius: 0,
+                marginLeft: 20,
+                marginRight: -20,
+                transform: "skewX(-30deg)",
+            },
+            "& $tabWrapper": {
+                textAlign: "end",
+                transform: "skewX(30deg)",
+            },
+            flexDirection: "column-reverse",
+            width: 264,
+        },
+        "tabsFlexContainer-left-vbox": {
+            "& $tabRoot": {
+                "&$activeTabRoot": {
+                    borderLeft: `2px solid ${theme.palette.secondary.main}`,
+                    borderTop: `2px solid ${theme.palette.secondary.main}`,
+                },
+                borderLeft: `1px solid ${theme.palette.secondary.main}`,
+                borderRight: 0,
+                borderTopLeftRadius: 6,
+                borderTopRightRadius: 0,
+                height: "auto",
+                marginBottom: 15,
+                minWidth: 36,
+                padding: "10px 0",
+                transform: "skewY(-30deg)",
+                width: 36,
+            },
+            "& $tabWrapper": {
+                maxHeight: 234,
+                textAlign: "start",
+                transform: "skewY(30deg) rotate(180deg)",
+                writingMode: "vertical-rl",
+            },
+            flexDirection: "column-reverse",
+            paddingTop: 10,
             width: 36,
         },
-        "& $tabWrapper": {
-            maxHeight: 234,
-            textAlign: "start",
-            transform: "skewY(-30deg)",
-            writingMode: "vertical-rl",
+        "tabsFlexContainer-right-hbox": {
+            "& $tabRoot": {
+                "&$activeTabRoot": {
+                    borderRight: `2px solid ${theme.palette.secondary.main}`,
+                    borderTop: `1px solid ${theme.palette.secondary.main}`,
+                },
+                "&:last-child": {
+                    "&$activeTabRoot": {
+                        borderTop: `2px solid ${theme.palette.secondary.main}`,
+                    },
+                    borderTop: `1px solid ${theme.palette.secondary.main}`,
+                },
+                borderBottom: `1px solid ${theme.palette.secondary.main}`,
+                borderTop: "none",
+                marginLeft: -20,
+                transform: "skewX(30deg)",
+            },
+            "& $tabWrapper": {
+                textAlign: "start",
+                transform: "skewX(-30deg)",
+            },
+            flexDirection: "column-reverse",
+            width: 264,
         },
-        flexDirection: "column-reverse",
-        paddingTop: 10,
-        width: 36,
-    },
-    tabsIndicator: {
-        display: "none",
-    },
-    tabsRoot: {
-        display: "block",
-        marginBottom: -5,
-        minHeight: 38,
-    },
-});
+        "tabsFlexContainer-right-vbox": {
+            "& $tabRoot": {
+                "&$activeTabRoot": {
+                    borderRight: `2px solid ${theme.palette.secondary.main}`,
+                    borderTop: `2px solid ${theme.palette.secondary.main}`,
+                },
+                height: "auto",
+                marginBottom: 15,
+                minWidth: 36,
+                padding: "10px 0",
+                transform: "skewY(30deg)",
+                width: 36,
+            },
+            "& $tabWrapper": {
+                maxHeight: 234,
+                textAlign: "start",
+                transform: "skewY(-30deg)",
+                writingMode: "vertical-rl",
+            },
+            flexDirection: "column-reverse",
+            paddingTop: 10,
+            width: 36,
+        },
+        tabsIndicator: {
+            display: "none",
+        },
+        tabsRoot: {
+            display: "block",
+            marginBottom: -5,
+            minHeight: 38,
+        },
+    };
+};

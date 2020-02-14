840b630747b31af056da1a010e7d38ff48ca398b
Oleh Kutsenko <inthedark1225@gmail.com>
2020-02-14 16:51:13 +0200

    CORE-1591 fixed freeze for collapsed panel

--- packages/@essence/essence-constructor-classes/src/PagesTree/components/TreeRow/TreeRow.styles.ts
+++ packages/@essence/essence-constructor-classes/src/PagesTree/components/TreeRow/TreeRow.styles.ts
@@ -1,55 +1,60 @@
 import {IEssenceTheme} from "@essence-community/constructor-share";
 import {makeStyles} from "@material-ui/core/styles";
 
-export const useStyles = makeStyles((theme: IEssenceTheme) => ({
-    chevronRoot: {
-        boxSizing: "content-box",
-        width: 17,
-    },
-    favoriteRoot: {
-        alignItems: "center",
-        bottom: 0,
-        display: "none",
-        left: 10,
-        position: "absolute",
-        top: 0,
-    },
-    favoriteSelected: {
-        "&$favoriteRoot": {
-            display: "flex",
+export const useStyles = makeStyles(
+    (theme: IEssenceTheme) => ({
+        chevronRoot: {
+            boxSizing: "content-box",
+            width: 17,
         },
-    },
-    folrderRoot: {
-        boxSizing: "content-box",
-        width: 23,
-    },
-    iconRoot: {
-        "&:empty": {
-            display: "block",
+        favoriteRoot: {
+            alignItems: "center",
+            bottom: 0,
+            display: "none",
+            left: 10,
+            position: "absolute",
+            top: 0,
         },
-        boxSizing: "content-box",
-        height: 23,
-        textAlign: "center",
-        width: 23,
-    },
-    nameTypography: {
-        fontSize: 15,
-    },
-    root: {
-        "&:hover": {
-            "& $favoriteRoot": {
+        favoriteSelected: {
+            "&$favoriteRoot": {
                 display: "flex",
             },
-            backgroundColor: theme.essence.palette.common.selectedMenu,
         },
-        color: theme.essence.palette.common.white,
-        cursor: "pointer",
-        position: "relative",
+        folrderRoot: {
+            boxSizing: "content-box",
+            width: 23,
+        },
+        iconRoot: {
+            "&:empty": {
+                display: "block",
+            },
+            boxSizing: "content-box",
+            height: 23,
+            textAlign: "center",
+            width: 23,
+        },
+        nameTypography: {
+            fontSize: 15,
+        },
+        root: {
+            "&:hover": {
+                "& $favoriteRoot": {
+                    display: "flex",
+                },
+                backgroundColor: theme.essence.palette.common.selectedMenu,
+            },
+            color: theme.essence.palette.common.white,
+            cursor: "pointer",
+            position: "relative",
+        },
+        rootGrid: {
+            marginBottom: 0,
+            marginTop: 0,
+            minHeight: 42,
+            padding: "0 10px",
+        },
+    }),
+    {
+        name: "EssencePagesTreeTreeRow",
     },
-    rootGrid: {
-        marginBottom: 0,
-        marginTop: 0,
-        minHeight: 42,
-        padding: "0 10px",
-    },
-}));
+);

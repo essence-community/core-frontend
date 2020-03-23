775f24528d05a1c48520f44185d77f9133799959
Oleh Kutsenko <inthedark1225@gmail.com>
2020-03-23 14:32:24 +0200

    CORE-1670 added file-loader for scripts

--- packages/@essence/essence-constructor-scripts/config/webpack.config.js
+++ packages/@essence/essence-constructor-scripts/config/webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 /* eslint-disable require-unicode-regexp, prefer-named-capture-group */
 /* eslint-disable no-sync, sort-keys, global-require */
 const path = require("path");
@@ -182,6 +183,30 @@ module.exports = {
                     },
                 ].filter(Boolean),
             },
+            /*
+             * "file" loader makes sure those assets get served by WebpackDevServer.
+             * When you `import` an asset, you get its (virtual) filename.
+             * In production, they would get copied to the `build` folder.
+             * This loader doesn't use a "test" so it will catch all modules
+             * that fall through the other loaders.
+             */
+            {
+                loader: require.resolve("file-loader"),
+                /*
+                 * Exclude `js` files to keep "css" loader working as it injects
+                 * its runtime that would otherwise be processed through "file" loader.
+                 * Also exclude `html` and `json` extensions so they get processed
+                 * by webpacks internal loaders.
+                 */
+                exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+                options: {
+                    name: "[name].[hash:8].[ext]",
+                },
+            },
+            /*
+             * ** STOP ** Are you adding a new loader?
+             * Make sure to add the new loader(s) before the "file" loader.
+             */
         ],
     },
     optimization: {

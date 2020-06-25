c2d2b631cc4ba205b901261471ad91a695890494
Oleh Kutsenko <inthedark1225@gmail.com>
2020-06-25 13:35:04 +0300

    CORE-1829 search restore value before select

--- packages/@essence/essence-constructor-classes/src/FieldTable/stores/FieldTableModel/FieldTableModel.ts
+++ packages/@essence/essence-constructor-classes/src/FieldTable/stores/FieldTableModel/FieldTableModel.ts
@@ -186,7 +186,7 @@ export class FieldTableModel extends StoreBaseModel implements IFieldTableModel 
                 );
                 this.selectedEntries.replace(getRestoredRecords(value, this));
             }
-        } else if (this.recordsStore.selectedRecord) {
+        } else {
             await this.recordsStore.searchAction(
                 {},
                 {
@@ -194,7 +194,10 @@ export class FieldTableModel extends StoreBaseModel implements IFieldTableModel 
                     selectedRecordId: value as string,
                 },
             );
-            this.handleChangeRecord(this.recordsStore.selectedRecord);
+
+            if (this.recordsStore.selectedRecord) {
+                this.handleChangeRecord(this.recordsStore.selectedRecord);
+            }
         }
         this.setRecordToGlobal();
     };

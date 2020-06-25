155c59eb04cf3f928a9408cc2b0f1991e873c454
Oleh Kutsenko <inthedark1225@gmail.com>
2020-06-25 15:46:02 +0300

    CORE-1823 common decorator for field repeator

--- packages/@essence/essence-constructor-classes/src/FieldRepeater/index.ts
+++ packages/@essence/essence-constructor-classes/src/FieldRepeater/index.ts
@@ -1,4 +1,5 @@
-import {setComponent} from "@essence-community/constructor-share";
+import {setComponent} from "@essence-community/constructor-share/components";
+import {commonDecorator} from "@essence-community/constructor-share/decorators";
 import {FieldRepeaterContainer} from "./containers/FieldRepeaterContainer";
 
-setComponent("IFIELD.repeater", FieldRepeaterContainer);
+setComponent("IFIELD.repeater", commonDecorator(FieldRepeaterContainer));

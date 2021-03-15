0557dccf12054851e13853278c24d9496afac0bc
Ivan Artemov <honyrik@gmail.com>
2021-03-15 18:29:51 +0300

    hot fix

--- packages/@essence/essence-constructor-scripts/config/webpack.config.js
+++ packages/@essence/essence-constructor-scripts/config/webpack.config.js
@@ -326,7 +326,7 @@ module.exports = {
                 "@essence-community",
                 "constructor-dll",
                 "dist",
-                "manifest.json",
+                "manifest_essence.json",
             )),
             name: "essenceconstructorshare",
         }),

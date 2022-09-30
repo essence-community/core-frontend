05224becd2f01fe513be4fe3a5678bebe4ce1370
Ivan Artemov <honyrik@gmail.com>
2022-09-30 14:33:47 +0300

    Fix example

--- packages/@essence/essence-constructor-scripts/template_example/src/components/ExampleBox/ExampleBox.styles.ts
+++ packages/@essence/essence-constructor-scripts/template_example/src/components/ExampleBox/ExampleBox.styles.ts
@@ -1,11 +1,16 @@
 import {makeStyles} from "@material-ui/core";
 import {IEssenceTheme} from "@essence-community/constructor-share/types";
 
-export const useStyles = makeStyles((theme: IEssenceTheme) => ({
-    "color-primary": {
-        border: `1 px solid ${theme.palette.primary.main}`,
+export const useStyles = makeStyles(
+    (theme: IEssenceTheme) => ({
+        "color-primary": {
+            border: `1 px solid ${theme.palette.primary.main}`,
+        },
+        "color-secondary": {
+            border: `1 px solid ${theme.palette.primary.main}`,
+        },
+    }),
+    {
+        name: "EssenceExampleBox",
     },
-    "color-secondary": {
-        border: `1 px solid ${theme.palette.primary.main}`,
-    },
-}));
+);

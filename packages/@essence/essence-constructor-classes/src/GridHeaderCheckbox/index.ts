87d19f2820892e547fdb5f01f7e863e85fcc30f2
Ivan Artemov <honyrik@gmail.com>
2022-12-02 14:37:09 +0300

    Fixed CHECKBOX

--- packages/@essence/essence-constructor-classes/src/GridHeaderCheckbox/index.ts
+++ packages/@essence/essence-constructor-classes/src/GridHeaderCheckbox/index.ts
@@ -1,4 +1,5 @@
 import {setComponent} from "@essence-community/constructor-share/components";
+import {commonDecorator} from "@essence-community/constructor-share/decorators";
 import {GridHeaderCheckboxContainer} from "./containers/GridHeaderCheckboxContainer";
 
-setComponent("GRID_HEADER.CHECKBOX", GridHeaderCheckboxContainer);
+setComponent("GRID_HEADER.CHECKBOX", commonDecorator(GridHeaderCheckboxContainer));

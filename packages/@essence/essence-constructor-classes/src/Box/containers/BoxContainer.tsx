f795dcbd15453059eed09cb7a56e0d10bf382a68
Ivan Artemov <honyrik@gmail.com>
2023-03-27 16:15:07 +0300

    Added attr align and box

--- packages/@essence/essence-constructor-classes/src/Box/containers/BoxContainer.tsx
+++ packages/@essence/essence-constructor-classes/src/Box/containers/BoxContainer.tsx
@@ -31,7 +31,7 @@ export const BoxContainer: React.FC<IClassProps> = (props) => {
             style={contentStyle}
             {...((contentview && GRID_CONFIGS[contentview]) || GRID_CONFIGS.vbox)}
             {...((contentview && align && GRID_ALIGN_CONFIGS[`${align}-${contentview}`]) ||
-                GRID_ALIGN_CONFIGS["left-hbox"])}
+                GRID_ALIGN_CONFIGS["left-vbox"])}
         >
             {children
                 ? children

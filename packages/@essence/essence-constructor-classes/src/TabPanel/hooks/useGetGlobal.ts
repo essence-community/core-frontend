7f852b46c70d20fbc2e2c404e5769bd1128a61c0
Ivan Artemov <honyrik@gmail.com>
2024-02-28 10:19:30 +0300

    Fixed getglobal tab panel

--- packages/@essence/essence-constructor-classes/src/TabPanel/hooks/useGetGlobal.ts
+++ packages/@essence/essence-constructor-classes/src/TabPanel/hooks/useGetGlobal.ts
@@ -1,4 +1,4 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {IBuilderConfig} from "@essence-community/constructor-share/types";
 import {VAR_RECORD_NAME, VAR_RECORD_PAGE_OBJECT_ID} from "@essence-community/constructor-share/constants";
 import {reaction} from "mobx";
@@ -13,22 +13,34 @@ interface IUseTabProps {
 export function useGetGlobal(props: IUseTabProps) {
     const {store} = props;
     const getValueGlobal = useGetValue({pageStore: store.pageStore});
+    const [, setLastActiveGlobal] = useState<string>(null);
 
     useEffect(() => {
         if (store.bc.getglobal) {
             return reaction(
-                () => parseMemoize(store.bc.getglobal).runer({get: getValueGlobal}),
-                (value) => {
-                    if (isEmpty(value)) {
-                        return;
-                    }
+                () => {
+                    const value = parseMemoize(store.bc.getglobal).runer({get: getValueGlobal});
                     const foundRec = store.childs.find(
                         (val) => val.ckobject === value || val[VAR_RECORD_NAME] === value,
                     );
 
-                    if (foundRec && !store.tabStatus[foundRec[VAR_RECORD_PAGE_OBJECT_ID]].hidden) {
-                        store.setActiveTab(foundRec[VAR_RECORD_PAGE_OBJECT_ID]);
+                    return foundRec && !store.tabStatus[foundRec[VAR_RECORD_PAGE_OBJECT_ID]].hidden
+                        ? foundRec[VAR_RECORD_PAGE_OBJECT_ID]
+                        : undefined;
+                },
+                (value) => {
+                    if (isEmpty(value)) {
+                        setLastActiveGlobal(null);
+
+                        return;
                     }
+                    setLastActiveGlobal((val) => {
+                        if (val !== value) {
+                            store.setActiveTab(value);
+                        }
+
+                        return value;
+                    });
                 },
                 {
                     fireImmediately: true,
